5b0a7e95276b2e6d25dec1cf7da309d0cff7114c
Touterima <rtoute311@gmail.com>
2025-06-23 11:40:29 +0200

    complete step one

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -3,8 +3,15 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-const getAllPhrases = function() { 
-  // TODO - your code here!
+
+const getAllPhrases = function(callback) {
+  const query = 'SELECT * FROM phrases';
+  connection.query(query, (err, results) => {
+    if (err) {
+      return callback(err); 
+    }
+    callback(null, results); 
+  });
 };
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const db = require('../database-mysql');
 
+
 const app = express();
 const PORT = 3000;
 
@@ -10,10 +11,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // UNCOMMENT TO START
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.json());
 
 app.get('/api/phrases', (req, res) => {
   //TODO - your code here!
+  db.getAllPhrases((err, phrases) => {
+    if (err) {
+      console.error('Erreur MySQL :', err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(phrases);
+    }
+  });
 });
 
 //TODO - add additional route handlers as necessary
@@ -21,3 +31,9 @@ app.get('/api/phrases', (req, res) => {
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+
+
+
+
+
